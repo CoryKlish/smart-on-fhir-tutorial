231a2f688eac6feba4986bbe61391bbed343d370
Cory Klish <Cory.Klish@cerner.com>
2020-04-15 15:09:01 -0400

    Get all needed values

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -79,19 +79,16 @@ var getData = function (client) {
     query.set("patient", client.patient.id);
     query.set("_count", 100); // Try this to fetch fewer pages
     query.set("code", [
-        'http://loinc.org|29463-7', // weight
-        'http://loinc.org|3141-9' , // weight
-        'http://loinc.org|8302-2' , // Body height
-        'http://loinc.org|8306-3' , // Body height --lying
-        'http://loinc.org|8287-5' , // headC
-        'http://loinc.org|39156-5', // BMI 39156-5
-        'http://loinc.org|18185-9', // gestAge
-        'http://loinc.org|37362-1', // bone age
-        'http://loinc.org|11884-4'  // gestAge
+      'http://loinc.org|8302-2',
+      'http://loinc.org|8462-4',
+      'http://loinc.org|8480-6',
+      'http://loinc.org|2085-9',
+      'http://loinc.org|2089-1',
+      'http://loinc.org|55284-4'
     ].join(","));
     var obv = client.request("Observation?" + query, {
-        pageLimit: 0,   // get all pages
-        flat     : true // return flat array of Observation resources
+      pageLimit: 0,   // get all pages
+      flat: true // return flat array of Observation resources
     });
 
     Promise.all([pt, obv]).then(function (values) {
@@ -100,10 +97,6 @@ var getData = function (client) {
       console.log(obv);
       var byCodes = client.byCodes(obv, 'code');
       var gender = patient.gender;
-
-      console.log('byCodes:');
-      console.log(byCodes("29463-7"));
-      console.log(byCodes("2345-7"));
 
       var fname = '';
       var lname = '';
